73f1903311cdfbbfc1f606f8fe8a6f64736704b8
raberin <rmvaberin@gmail.com>
2020-01-29 16:01:39 -0800

    Complete Day 1 MVP of BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
+# from dll_stack import Stack
+# from dll_queue import Queue
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,31 +12,86 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # Check if the value is duplicate, return False
+        if self.value == value:
+            return False
+        # If value to be inserted, is less than node.value
+        elif value < self.value:
+            # If there is a left child, recursively call insert
+            # Otherwise insert to the left of current node
+            if self.left:
+                return self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+                return True
+        else:
+            # If there is a right child, recursively call insert
+            # Otherwise insert to the right of current node
+            if self.right:
+                return self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+                return True
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # Base Case Return True when value == target
+        if self.value == target:
+            return True
+        else:
+            # Recursive Cases
+            # If target is greater and there is a right node recurse right
+            # Otherwise, return false
+            if target > self.value:
+                if self.right:
+                    return self.right.contains(target)
+                else:
+                    return False
+            # If target is less than and there is a left node recurse left
+            # Otherwise, return false
+            else:
+                if self.left:
+                    return self.left.contains(target)
+                else:
+                    return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # While there are right child nodes, move right
+        while self.right:
+            self = self.right
+        # Return value
+        return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
+
     def for_each(self, cb):
-        pass
+        # Run func in current node
+        cb(self.value)
+        # If left child present, recursively run func to left nodes
+        if self.left:
+            self.left.for_each(cb)
+        # If right child present, recursively run func to right nodes
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
-        pass
+        if self.left:
+            self.left.in_order_print(node.left.value)
+        print(node.value)
+        if self.right:
+            self.right.in_order_print(node.right.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+
     def bft_print(self, node):
         pass
 
